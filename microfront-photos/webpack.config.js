978f230746b8eb519b0a2058d53a7c9404009b9a
Stiven Gomez <stivenalexander7u7@gmail.com>
2024-04-07 22:33:12 -0500

    FIX - Ajuste de configuracón WebPack módulo Photos.

--- microfront-photos/webpack.config.js
+++ microfront-photos/webpack.config.js
@@ -1,15 +1,22 @@
-const { shareAll, withModuleFederationPlugin } = require('@angular-architects/module-federation/webpack');
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
 
-module.exports = withModuleFederationPlugin({
-
-  name: 'microfront-photos',
-
+const moduleFederationConfig = withModuleFederationPlugin({
+  name: "mf-photo",
   exposes: {
-    './Component': './src/app/app.component.ts',
+    "./PhotoModule": "./src/app/photo/photo.module.ts",
   },
 
   shared: {
-    ...shareAll({ singleton: true, strictVersion: true, requiredVersion: 'auto' }),
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
   },
-
 });
+
+moduleFederationConfig.output.publicPath = "http://localhost:4201/";
+module.exports = moduleFederationConfig;
