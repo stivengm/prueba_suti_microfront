8253e0d5d915b6840d2f2dc7deefc62341a5b2c9
Stiven Gomez <stivenalexander7u7@gmail.com>
2024-04-07 23:41:26 -0500

    FIX - Ajuste de servicio POSTS para endpoint desde environments.

--- microfront-posts/src/app/core/services/get-posts.service.ts
+++ microfront-posts/src/app/core/services/get-posts.service.ts
@@ -2,17 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { PostsInterface } from '../models/posts.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetPostsService {
 
-  url = "https://jsonplaceholder.typicode.com/";
-
   constructor(private http: HttpClient) { }
 
   getPosts(): Observable<PostsInterface[]> {
-    return this.http.get<PostsInterface[]>(this.url + `/posts`);
+    return this.http.get<PostsInterface[]>(environment.apiPosts + `/posts`);
   }
 }
--- microfront-posts/src/environments/environment.ts
+++ microfront-posts/src/environments/environment.ts
@@ -1,1 +1,4 @@
-export const environment = {};
+export const environment = {
+    production: false,
+    apiPosts: "https://jsonplaceholder.typicode.com"
+};
