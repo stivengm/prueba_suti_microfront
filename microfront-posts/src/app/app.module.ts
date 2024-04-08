b24281fb1127f85b154da77a2c936c4133a27285
Stiven Gomez <stivenalexander7u7@gmail.com>
2024-04-07 23:35:58 -0500

    ADD - Se añade configuración WebPack Module-Federation en REMOTE POSTS.

--- microfront-posts/src/app/app.module.ts
+++ microfront-posts/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, 
+    PostsComponent, 
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ microfront-posts/webpack.config.js
@@ -0,0 +1,20 @@
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
+
+module.exports = withModuleFederationPlugin({
+  name: "mf-posts",
+
+  exposes: {
+    "./PostsComponent": "./src/app/posts/posts.component.ts",
+  },
+
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
+  },
+});
