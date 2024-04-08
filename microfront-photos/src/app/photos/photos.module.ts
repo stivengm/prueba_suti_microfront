d3293a187dc2f26344da9ffa358eec7ca99bfb55
Stiven Gomez <stivenalexander7u7@gmail.com>
2024-04-07 19:45:54 -0500

    ADD - Se añade componente y módulo photos.

---
+++ microfront-photos/src/app/photos/photos.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PhotosComponent } from './photos.component';
+
+
+
+@NgModule({
+  declarations: [
+    PhotosComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PhotosModule { }
