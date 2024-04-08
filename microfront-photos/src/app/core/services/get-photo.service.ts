1ed935ec72213903f90ea54315f630a5a63b0548
Stiven Gomez <stivenalexander7u7@gmail.com>
2024-04-07 23:43:06 -0500

    FIX - Ajuste de servicio photos para api desde environments.

--- microfront-photos/src/app/core/services/get-photo.service.ts
+++ microfront-photos/src/app/core/services/get-photo.service.ts
@@ -2,17 +2,16 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { PhotoInterface } from '../models/photo.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetPhotoService {
 
-  url = "https://jsonplaceholder.typicode.com/"
-
   constructor(private http: HttpClient) { }
 
   getPhotos(): Observable<PhotoInterface[]> {
-    return this.http.get<PhotoInterface[]>(this.url + `/photos`);
+    return this.http.get<PhotoInterface[]>(environment + `/photos`);
   }
 }
---
+++ microfront-photos/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: false,
+    apiPhotos: "https://jsonplaceholder.typicode.com"
+};
