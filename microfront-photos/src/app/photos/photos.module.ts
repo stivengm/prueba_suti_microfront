4b4bc30648675687d1964c242bf1940b13f3e49c
Stiven Gomez <stivenalexander7u7@gmail.com>
2024-04-07 19:48:42 -0500

    FIX - Ajuste de rutas en photos.module.

--- microfront-photos/src/app/photos/photos.module.ts
+++ microfront-photos/src/app/photos/photos.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PhotosComponent } from './photos.component';
 
+import { RouterModule, Routes } from '@angular/router';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: PhotosComponent
+  }
+]
 
 @NgModule({
   declarations: [
     PhotosComponent
   ],
   imports: [
+    RouterModule.forChild(routes),
     CommonModule
   ]
 })
